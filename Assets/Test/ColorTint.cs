313817f897c5a4b74f57876deb558ba649f10fa7
danshenmiao <wsdanshenmiao@outlook.com>
2025-01-24 22:32:19 +0800

    清理项目

--- Assets/Test/ColorTint.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
-
-public class ColorTint : VolumeComponent
-{
-    //【设置颜色参数】
-    public ColorParameter colorChange = new ColorParameter(Color.white, true);//如果有两个true,则为HDR设置
-}
